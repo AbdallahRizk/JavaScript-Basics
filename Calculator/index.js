239378789573b39519d56b95def29cdb844e5d72
Abdallah Rizk <abm.rizk@hotmail.com>
2019-03-13 11:55:09 +0300

    updated the calculator css and prepared the wanted functionality

---
+++ Calculator/index.js
@@ -0,0 +1,11 @@
+/* The functionality to programme:
+
+1- Every Button you press has to be typed in the textfield 
+except "Equal, Delete, CLear"
+2-BY pressing Equal show the result or an error massage
+3- By pressing Delete delete only one item 
+4- By pressing clear delete everything on the textField
+5- As long as there is no error just keep showing the result and keep calculating the result
+
+*/
+
