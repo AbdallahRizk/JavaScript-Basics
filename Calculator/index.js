7b73c4a70baafd7bd3e11a1752b462293b0f6085
Abdallah Rizk <abm.rizk@hotmail.com>
2019-03-14 13:53:51 +0300

    updated indexjs to prepare to do the calculation part

--- Calculator/index.js
+++ Calculator/index.js
@@ -21,7 +21,29 @@ function retunNumber(val) {
     }
 }
 
-document.getElementById('btn').addEventListener("click", function(){
+document.getElementById('clear').addEventListener("click", function(){
     document.getElementById('result').value= " ";
     textfield = " ";
+});
+
+document.getElementById('equal').addEventListener("click", function(){
+    // add all the numbers on an array of numbers with split tokens and add the tokens on another array (+,-,*,/) 
+    // then loop over all the array and math the first two items of the array with the first tokesn on the tokesn array
+    // then keep doint the math with the result with the next array item until you loop over all the array
+    // in case there is unfamiliarity with the buttons fire try and catch with an error massage
+    // in the end keep the result on the textbox to be ready for other math opperations
+
+    //so to staff to know first:
+    //how to create an arry
+    //how to read unitl specific token and split
+    //how to keep the token in another array
+    //how to loop over 2 array and use them togather
+    //how to fire try and catch once need it
+
+    var numbers = [];
+    var tokens = [];
+
+    while(0){
+        
+    }
 });
