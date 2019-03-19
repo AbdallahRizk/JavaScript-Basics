cc480407780019c1106b43d145297d1a4ff9d589
Abdallah Rizk <abm.rizk@gmail.com>
2019-03-19 16:40:01 +0300

    calculation part done

--- Calculator/index.js
+++ Calculator/index.js
@@ -15,65 +15,68 @@ function retunNumber(val) {
     if (textfield != " ") {
         textfield = textfield + val;
         document.getElementById('result').value = textfield;
-    } else if (textfield == " ") {
+    } else {
+        textfield == " "
         textfield = val;
         document.getElementById('result').value = textfield;
     }
 }
 
-document.getElementById('clear').addEventListener("click", function(){
-    document.getElementById('result').value= " ";
+
+document.getElementById('clear').addEventListener("click", function () {
+    document.getElementById('result').value = " ";
     textfield = " ";
 });
 
 
 // add all the numbers on an array of numbers with split tokens and add the tokens on another array (+,-,*,/) 
-    // then loop over all the array and math the first two items of the array with the first tokesn on the tokesn array
-    // then keep doint the math with the result with the next array item until you loop over all the array
-    // in case there is unfamiliarity with the buttons fire try and catch with an error massage
-    // in the end keep the result on the textbox to be ready for other math opperations
+// then loop over all the array and math the first two items of the array with the first tokesn on the tokesn array
+// then keep doint the math with the result with the next array item until you loop over all the array
+// in case there is unfamiliarity with the buttons fire try and catch with an error massage
+// in the end keep the result on the textbox to be ready for other math opperations
 
-    //so to staff to know first:
-    //how to create an arry
-    //how to read unitl specific token and split
-    //how to keep the token in another array
-    //how to loop over 2 array and use them togather
-    //how to fire try and catch once need it
+//so to staff to know first:
+//how to create an array
+//how to read unitl specific token and split
+//how to keep the token in another array
+//how to fire try and catch once need it
 
-function result(numbers, operations){
+function result(numbers, operations) {
 
-    var currentResult = numbers[0];
-    for(var i =0; i<= operations.length ; i++){
-        if(operations[i] == '+'){
-            currentResult = currentResult + numbers[i+1];
-        }else if(operations[i] == '-'){
-            currentResult = currentResult - numbers[i+1];
-        }else if(operations[i] == '*'){
-            currentResult = currentResult * numbers[i+1];
-        }if(operations[i] == '/'){
-            currentResult = currentResult / numbers[i+1];
-        }else{
+    var currentResult = Number(numbers[0]);
+    for (var i = 0; i < operations.length; i++) {
+        if (operations[i] == '+') {
+            currentResult = currentResult + Number(numbers[i + 1]);
+        } else if (operations[i] == '-') {
+            currentResult = currentResult - Number(numbers[i + 1]);
+        } else if (operations[i] == '*') {
+            currentResult = currentResult * Number(numbers[i + 1]);
+        } else if (operations[i] == '/') {
+            currentResult = currentResult / Number(numbers[i + 1]);
+        } else {
             currentResult = "Wrong";
-        } 
-    }return currentResult;
+        }
+    }
+    return currentResult;
 }
 
-document.getElementById('equal').addEventListener("click", function(){
+document.getElementById('equal').addEventListener("click", function () {
 
     var numbers = textfield.split(/\D/g);
     var operations = textfield.split(/\d/g).filter(Boolean);
-    
+
     /*
     console.log(textfield);
     console.log(numbers);
     console.log(operations);
     */
-   if(Number.isInteger(result(numbers, operations)) == true){
+   
+if(isNaN(result(numbers, operations)) == false){
     textfield =result(numbers, operations);
    }else{
        textfield = "Invalid expression"
    }
-    
+   document.getElementById('result').value = textfield;
 
 
 });
